8eb15ce3b5225b3cc9f1de4fc0464f90410d424f
Evgeniy Andreev <gsomix@gmail.com>
2018-08-02 18:59:17 +0400

    Implement toggling of completed items

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -14,43 +14,63 @@ open Fulma.Extensions
 let [<Literal>] ENTER_KEY = 13.
 
 type Model = { 
-    TodoItems: list<Todo> 
+    TodoItems: Map<Id, Todo> 
     NewTodoDescription: string
+    LastId: Id
 }
 
 type Msg =
     | AddTodo
     | EditNewTodoDescription of string
+    | ToggleCompleted of Id
 
 let init () : Model * Cmd<Msg> =
     let initialModel = 
-        { TodoItems = []
-          NewTodoDescription = "" }
-
+        { TodoItems = Map.empty
+          NewTodoDescription = "" 
+          LastId = 0 }
     initialModel, Cmd.none
 
 let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
     match msg with
     | EditNewTodoDescription str ->
-        let newModel = { currentModel with NewTodoDescription = str }
+        let newModel = { currentModel with NewTodoDescription = str }        
         newModel, Cmd.none
         
     | AddTodo -> 
+        let newId = currentModel.LastId + 1 // TODO Move computation to Id type 
         let newItem = 
-            { Description = currentModel.NewTodoDescription
-              Completed = false }
-
+            { Todo.Empty with
+                Id = newId 
+                Description = currentModel.NewTodoDescription }
         let newModel =
             { currentModel with 
-                TodoItems = newItem :: currentModel.TodoItems
-                NewTodoDescription = "" }
-
+                TodoItems = currentModel.TodoItems |> Map.add newId newItem
+                NewTodoDescription = ""
+                LastId = newId }
         newModel , Cmd.none
 
-let showItems (items: list<Todo>) =
-    [ for item in items -> 
+    | ToggleCompleted id -> 
+        let item = Map.find id currentModel.TodoItems
+        let newItem = { item with Completed = not item.Completed }
+        let newModel = 
+            { currentModel with 
+                TodoItems = currentModel.TodoItems |> Map.add id newItem }
+        newModel, Cmd.none
+        
+
+let showItems (items: Map<Id, Todo>) (dispatch: Msg -> unit) =
+    [ for KeyValue(id, item) in items -> 
         Panel.block [ ] 
-            [ Checkradio.checkbox [ Checkradio.Checked item.Completed ] [ str item.Description ] ] ]
+            [ Checkradio.checkbox // TODO Fix styling
+                [ Checkradio.Checked item.Completed 
+                  Checkradio.OnChange (fun _ -> dispatch <| ToggleCompleted id)] 
+                [ str item.Description ] ] ]
+
+let countActive (items: Map<Id, Todo>) =
+    items
+    |> Map.filter (fun _ v -> not v.Completed)
+    |> Map.count
 
 let view (model: Model) (dispatch: Msg -> unit) =
     Columns.columns [ ]
@@ -66,8 +86,8 @@ let view (model: Model) (dispatch: Msg -> unit) =
                               Input.Props 
                                 [ OnInput (fun e -> dispatch <| EditNewTodoDescription e.Value) 
                                   OnKeyDown (fun e -> if e.which = ENTER_KEY then dispatch <| AddTodo) ] ] ] ] 
-                  yield! showItems (model.TodoItems)
-                  yield Panel.block [ ] [ str (sprintf "%d items left" model.TodoItems.Length) ] ] ] ]
+                  yield! showItems model.TodoItems dispatch
+                  yield Panel.block [ ] [ str (sprintf "%d items left" (countActive model.TodoItems)) ] ] ] ]
 
 
 #if DEBUG
--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -2,8 +2,17 @@ namespace Shared
 
 type Counter = int
 
-type Todo = { 
+type Id = int // TODO Make single DU type or use UoM
+
+type Todo = {
+    Id: Id 
     Description: string
     Completed: bool 
 }
+
+module Todo =
+    let Empty = 
+        { Id = 0
+          Description = ""
+          Completed = false }
 
