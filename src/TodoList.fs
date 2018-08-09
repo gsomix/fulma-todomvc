109762072f977ab9628d1d2b43f62bd1fa543df8
Evgeniy Andreev <gsomix@gmail.com>
2018-08-10 01:15:06 +0400

    Refactoring

--- src/TodoList.fs
+++ src/TodoList.fs
@@ -1,135 +1,143 @@
 namespace TodoList
 
-module Domain =
+module rec Domain =
     type Id = int
 
-    type TodoStatus =
-        | TodoActive
-        | TodoCompleted
-        with
-        member this.Swap =
-            match this with
-            | TodoActive -> TodoCompleted
-            | TodoCompleted -> TodoActive
+    type Todo =
+        { Id: Id
+          Description: string
+          Status: Todo.Status }
 
-    type Todo = {
-        Id: Id
-        Description: string
-        Status: TodoStatus
-    } with
-        member this.IsDone = this.Status = TodoCompleted
-        static member Empty = {
-            Id = 0
-            Description = ""
-            Status = TodoActive
-        }
+    type Todo with
+        static member Empty =
+            { Id = 0
+              Description = ""
+              Status = Todo.Active }
+
+    module Todo =
+        type Status =
+            | Active
+            | Completed
+
+        type Status with
+            member this.Toggle =
+                match this with
+                | Active -> Completed
+                | Completed -> Active
+
+        let isDone todo = todo.Status = Todo.Completed
 
 module Types =
     open Domain
 
-    type Filter = | All | Active | Completed
+    type Filter = All | Active | Completed
     type TodoList = Map<Id, Todo>
 
+    module Map =
+        let filterValues predicate table =
+            table |> Map.filter (fun _ v -> predicate v)
+
     module TodoList =
-        let filterItems (filter: Filter) (items: TodoList) =
+        let filterEntries (filter: Filter) (entries: TodoList) =
             match filter with
-            | All -> items
-            | Active -> items |> Map.filter (fun _ item -> not item.IsDone)
-            | Completed -> items |> Map.filter (fun _ item -> item.IsDone)
+            | All -> entries
+            | Active -> entries |> Map.filterValues (Todo.isDone >> not)
+            | Completed -> entries |> Map.filterValues Todo.isDone
 
-        let countItems (filter: Filter) (items: TodoList) =
-            items
-            |> filterItems filter
-            |> Map.count
+        let countEntries (filter: Filter) (entries: TodoList) =
+            entries |> filterEntries filter |> Map.count
 
-    type Model = {
-        TodoItems: TodoList
-        DescriptionField: string
-        IdCounter: Id
-        ActiveFilter: Filter
-        EditingItem: Todo option
-    }
+    type Model =
+        { Entries: TodoList
+          Counter: Id
+          Field: string
+          Filter: Filter
+          EditingEntry: Todo option
+        }
 
     type Msg =
         // Operations on todo list
-        | AddItem
-        | DeleteItem of Id
-        | StartEditItem of Id
-        | StopEditItem
-        | UpdateEditingItem of string
-        | ToggleCompleted of Id
-        | ClearCompleted
+        | Add
+        | Delete of Id
+        | DeleteComplete
+        | Check of Id
+
+        // Editing
+        | StartEditEntry of Id
+        | StopEditEntry
+        | UpdateEditingEntry of string
 
         // UI messages
-        | DescriptionFieldInput of string
-        | ActivateFilter of Filter
+        | UpdateField of string
+        | ChangeFilter of Filter
 
 module State =
     open Elmish
     open Domain
     open Types
 
-    let init () : Model * Cmd<Msg> =
-        let initialModel =
-            { TodoItems = Map.empty
-              DescriptionField = ""
-              IdCounter = 0
-              ActiveFilter = All
-              EditingItem = None }
-        initialModel, Cmd.none
+    let init () =
+        { Entries = Map.empty
+          Counter = 0
+          Field = ""
+          Filter = All
+          EditingEntry = None
+        }, Cmd.none
 
-    let update (msg : Msg) (curModel : Model) : Model * Cmd<Msg> =
+    let update (msg : Msg) (model : Model) =
         match msg with
-        | AddItem ->
-            if curModel.DescriptionField = ""
-            then curModel, Cmd.none
+        | Add ->
+            if model.Field = ""
+            then model, Cmd.none
             else
-                let id = curModel.IdCounter + 1
-                let item = { Todo.Empty with Id = id; Description = curModel.DescriptionField }
-                { curModel with
-                    TodoItems = curModel.TodoItems |> Map.add id item
-                    DescriptionField = ""
-                    IdCounter = id }, Cmd.none
+                let id = model.Counter + 1
+                let entry = { Todo.Empty with Id = id; Description = model.Field }
+                { model with
+                    Entries = model.Entries |> Map.add id entry
+                    Field = ""
+                    Counter = id
+                }, Cmd.none
 
-        | DeleteItem id ->
-            { curModel with TodoItems = curModel.TodoItems.Remove id }, Cmd.none
+        | Delete id ->
+            { model with Entries = model.Entries.Remove id }, Cmd.none
 
-        | StartEditItem id ->
-            { curModel with EditingItem = curModel.TodoItems.TryFind id }, Cmd.none
+        | DeleteComplete ->
+            let entries = model.Entries |> TodoList.filterEntries Active
+            { model with Entries = entries }, Cmd.none
 
-        | UpdateEditingItem str ->
-            let item =
-                curModel.EditingItem
-                |> Option.map (fun item -> { item with Description = str })
-            { curModel with EditingItem = item }, Cmd.none
+        | Check id ->
+            let entries =
+                model.Entries
+                |> Map.tryFind id
+                |> Option.map (fun entry ->
+                    let entry = { entry with Status = entry.Status.Toggle }
+                    model.Entries.Add (id, entry))
+            { model with Entries = defaultArg entries model.Entries }, Cmd.none
 
-        | StopEditItem ->
-            let items =
-                curModel.EditingItem
-                |> Option.map (fun item -> if item.Description = ""
-                                           then curModel.TodoItems.Remove item.Id
-                                           else curModel.TodoItems.Add (item.Id, item))
-            { curModel with
-                TodoItems = defaultArg items curModel.TodoItems
-                EditingItem = None }, Cmd.none
+        | StartEditEntry id ->
+            { model with EditingEntry = model.Entries.TryFind id }, Cmd.none
 
-        | ToggleCompleted id ->
-            let items =
-                curModel.TodoItems.TryFind id
-                |> Option.map (fun item -> { item with Status = item.Status.Swap })
-                |> Option.map (fun item -> curModel.TodoItems.Add (id, item))
-            { curModel with
-                TodoItems = defaultArg items curModel.TodoItems}, Cmd.none
+        | UpdateEditingEntry str ->
+            let entry =
+                model.EditingEntry
+                |> Option.map (fun entry -> { entry with Description = str })
+            { model with EditingEntry = entry }, Cmd.none
 
-        | ClearCompleted ->
-            let item = curModel.TodoItems |> Map.filter (fun _ item -> not item.IsDone)
-            { curModel with TodoItems = item }, Cmd.none
+        | StopEditEntry ->
+            let entries =
+                model.EditingEntry
+                |> Option.map (fun entry -> if entry.Description = ""
+                                            then model.Entries.Remove entry.Id
+                                            else model.Entries.Add (entry.Id, entry))
+            { model with
+                Entries = defaultArg entries model.Entries
+                EditingEntry = None }, Cmd.none
 
-        | DescriptionFieldInput str ->
-            { curModel with DescriptionField = str }, Cmd.none
+        | UpdateField str ->
+            { model with Field = str }, Cmd.none
 
-        | ActivateFilter tab ->
-            { curModel with ActiveFilter = tab }, Cmd.none
+        | ChangeFilter tab ->
+            { model with Filter = tab }, Cmd.none
 
 module View =
     open Fable.Helpers.React
@@ -143,107 +151,121 @@ module View =
     let [<Literal>] ENTER_KEY = 13.
 
     let view (model: Model) (dispatch: Msg -> unit) =
-        let filtered = TodoList.filterItems model.ActiveFilter model.TodoItems
+        let viewEntry entry isEditing =
+            let color status =
+                if status = Todo.Completed
+                then Color.IsGreyLight
+                else Color.IsBlack
 
-        let pluralize count =
-            sprintf "%d %s" count (if count = 1 then "item" else "items")
-
-        let statusColor status =
-            if status = TodoCompleted
-            then Color.IsGreyLight
-            else Color.IsBlack
-
-        let viewItem item isEditing =
-            let control =
-                if isEditing then
-                    Input.text
-                        [ Input.Size Size.IsSmall
-                          Input.Value item.Description
-                          Input.Props
-                            [ OnBlur    (fun _ -> StopEditItem |> dispatch)
-                              OnChange  (fun e -> e.Value |> UpdateEditingItem |> dispatch)
-                              OnKeyDown (fun e -> if e.which = ENTER_KEY then StopEditItem |> dispatch)
-                              AutoFocus true
-                            ]
+            let input =
+                Input.text
+                    [ Input.Size Size.IsSmall
+                      Input.Value entry.Description
+                      Input.Props
+                        [ OnBlur    (fun _ -> StopEditEntry |> dispatch)
+                          OnChange  (fun e -> e.Value |> UpdateEditingEntry |> dispatch)
+                          OnKeyDown (fun e -> if e.which = ENTER_KEY then StopEditEntry |> dispatch)
+                          AutoFocus true
                         ]
-                else
-                    Text.p
-                        [ Props     [ OnDoubleClick (fun _ -> StartEditItem item.Id |> dispatch) ]
-                          Modifiers [ Modifier.TextColor (statusColor item.Status) ]
-                        ]
-                        [ str item.Description ]
+                    ]
+
+            let description =
+                Text.p
+                    [ Modifiers [ Modifier.TextColor (color entry.Status) ] ]
+                    [ str entry.Description ]
 
             Level.level
-                [ Level.Level.Props [ Style [ Flex "auto" ] ] ]
+                [ Level.Level.Props [ Style [ Flex "auto" ] ]
+                  Level.Level.IsMobile
+                ]
                 [ Level.left [ ]
                     [ Level.item [ ]
                         [ Checkradio.checkbox
-                            [ Checkradio.Checked item.IsDone
-                              Checkradio.OnChange (fun _ -> ToggleCompleted item.Id |> dispatch)
+                            [ Checkradio.Checked (Todo.isDone entry)
+                              Checkradio.OnChange (fun _ -> Check entry.Id |> dispatch)
                             ]
                             [ ]
                         ]
-                      Level.item [ ] [ control ]
+                      Level.item [ ] [ (if isEditing then input else description) ]
                     ]
 
                   Level.right [ ]
                     [ Delete.delete
                         [ Delete.Modifiers [ Modifier.IsPulledRight ]
-                          Delete.OnClick (fun _ -> DeleteItem item.Id |> dispatch)
+                          Delete.OnClick (fun _ -> Delete entry.Id |> dispatch)
                         ]
                         [ ]
                     ]
                 ]
 
-        let viewItems model =
-            [ for KeyValue(_, item) in model.TodoItems ->
-                match model.EditingItem with
-                | Some edtItem when edtItem.Id = item.Id ->
-                    Panel.block [ ] [ viewItem edtItem true ]
+        let viewEntries entries editingEntry =
+            [ for KeyValue(_, entry) in entries ->
+                match editingEntry with
+                | Some editingEntry when editingEntry.Id = entry.Id ->
+                    Panel.block [ ] [ viewEntry editingEntry true ]
                 | _ ->
-                    Panel.block [ ] [ viewItem item false ]
+                    Panel.block
+                        [ Panel.Block.Props [OnDoubleClick (fun _ -> StartEditEntry entry.Id |> dispatch)] ]
+                        [ viewEntry entry false ]
             ]
 
         let viewInput placeholder value =
-            Input.text
-                [ Input.Placeholder placeholder
-                  Input.Value value
-                  Input.Props
-                    [ OnChange  (fun field -> DescriptionFieldInput field.Value |> dispatch)
-                      OnKeyDown (fun e -> if e.which = ENTER_KEY then AddItem |> dispatch)
+            Panel.block [ ]
+                [ Input.text
+                    [ Input.Placeholder placeholder
+                      Input.Value value
+                      Input.Props
+                        [ OnChange  (fun field -> UpdateField field.Value |> dispatch)
+                          OnKeyDown (fun key -> if key.which = ENTER_KEY then Add |> dispatch)
+                        ]
                     ]
                 ]
 
         let viewTab tab activeTab =
             Panel.tab
                 [ Panel.Tab.IsActive (activeTab = tab)
-                  Panel.Tab.Props [ OnClick (fun _ -> ActivateFilter tab |> dispatch) ]
+                  Panel.Tab.Props [ OnClick (fun _ -> ChangeFilter tab |> dispatch) ]
                 ]
                 [ str (sprintf "%A" tab) ]
 
-        let viewTabs tabs activeTab = [ for tab in tabs -> viewTab tab activeTab ]
+        let viewTabs tabs activeTab =
+            Panel.tabs [ ] [ for tab in tabs -> viewTab tab activeTab ]
 
-        Panel.panel [ ]
-            [ yield Panel.heading [ ] [ str "Todos" ]
-              yield Panel.block [ ] [ viewInput "What needs to be done?" model.DescriptionField ]
-              yield Panel.tabs [ ] [ yield! viewTabs [All; Active; Completed] model.ActiveFilter ]
-              yield! viewItems { model with TodoItems = filtered }
-              yield Panel.block [ ]
+        let viewControls =
+            let pluralize count =
+                sprintf "%d %s" count (if count = 1 then "item" else "items")
+
+            let activeEntries = model.Entries |> TodoList.countEntries Active
+            let completedEntries = model.Entries |> TodoList.countEntries Completed
+
+            Panel.block [ ]
                 [ Level.level
-                    [ Level.Level.Props [ Style [ Flex "auto" ] ] ]
+                    [ Level.Level.Props [ Style [ Flex "auto" ] ]
+                      Level.Level.IsMobile
+                    ]
                     [ Level.left [ ]
-                        [ Text.div [ ]
-                            [ str (sprintf "%s left" (pluralize (model.TodoItems |> TodoList.countItems Active))) ] ]
+                        [ Text.p [ ] [ str (sprintf "%s left" (pluralize activeEntries)) ] ]
 
                       Level.right [ ]
                         [ Button.button
                             [ Button.Modifiers [ Modifier.IsPulledRight ]
                               Button.Size IsSmall
-                              Button.Disabled (model.TodoItems |> TodoList.countItems Completed = 0)
-                              Button.OnClick (fun _ -> ClearCompleted |> dispatch)
+                              Button.Disabled (completedEntries = 0)
+                              Button.OnClick (fun _ -> DeleteComplete |> dispatch)
                             ]
                             [ str "Clear completed" ]
                         ]
                     ]
                 ]
+
+        let viewHeading title = Panel.heading [ ] [ str title ]
+
+        let filtered = model.Entries |> TodoList.filterEntries model.Filter
+
+        Panel.panel [ ]
+            [ yield  viewHeading "Todos"
+              yield  viewInput "What needs to be done?" model.Field
+              yield  viewTabs [All; Active; Completed] model.Filter
+              yield! viewEntries filtered model.EditingEntry
+              yield  viewControls
             ]
